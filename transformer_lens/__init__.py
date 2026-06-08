bb409edd47fa97d0ed58bdfb2ebec51be4f26c58
Augusto Nunes <nunes.augusto19@gamil.com>
2026-06-08 04:55:09 -0300

    Add BertForTokenClassification

--- transformer_lens/__init__.py
+++ transformer_lens/__init__.py
@@ -13,6 +13,7 @@ from . import loading_from_pretrained as loading
 from . import supported_models
 from .ActivationCache import ActivationCache
 from .BertNextSentencePrediction import BertNextSentencePrediction
+from .BertForTokenClassification import BertForTokenClassification
 from .cache.key_value_cache import TransformerLensKeyValueCache
 from .cache.key_value_cache_entry import TransformerLensKeyValueCacheEntry
 from .config import HookedTransformerConfig, TransformerBridgeConfig
